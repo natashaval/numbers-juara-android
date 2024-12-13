92097071b55490dd7e0c0a9a78add891578c5e56
natashaval <natasha.valentina1998@gmail.com>
2024-12-13 05:53:38 +0000

    exclude compose first

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,11 +43,11 @@ android {
     }
     buildFeatures {
         viewBinding = true
-        compose = true
+//        compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.5.1"
+//    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
